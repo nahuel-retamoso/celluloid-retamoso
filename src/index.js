8fc1a32324aaf1b7e700f759a268868c6ef5b2ce
tumijiu <nahuel.retamoso@gmail.com>
2022-05-16 19:04:43 -0300

    Clase componentes 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar/NavBar';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
